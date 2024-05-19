342be998ceffab14ce1bcadbb402fbb8164c9212
wilmer_choxom <lorwilm@gmail.com>
2024-05-19 17:53:46 -0600

    agregando paquetes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import multimedia.Podcasts;
+import multimedia.Songs;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    Podcasts podcasts=new Podcasts();
+    Songs songs = new Songs();
+
 }
